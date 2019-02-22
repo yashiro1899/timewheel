c5bc2cdcb3a4a874144749830c45121391276112
Ice White <yashiro1899@gmail.com>
2019-02-22 19:36:14 +0800

    1550835374

--- timewheel.go
+++ timewheel.go
@@ -1,28 +1,69 @@
 package timewheel
 
-import "time"
+import (
+	"log"
+	"time"
+)
 
 type TimeWheel struct {
-	ticker    time.Ticker
-	interval  time.Duration
-	tolerance time.Duration
-	scale     int
-	current   int
-	slots     []map[uint64]*Task
-	next      *TimeWheel
+	ticker   *time.Ticker
+	interval time.Duration
+	scale    int
+	divisor  int
+	counter  int
+	current  int
+	slots    []map[uint64]*Task
+	next     *TimeWheel
 
 	addTaskC    chan *Task
 	removeTaskC chan uint64
 	stopC       chan bool
 }
 
-func NewTimeWheel(interval, tolerance time.Duration, scale int) *TimeWheel {
+func NewTimeWheel(interval time.Duration, scale, divisor int) *TimeWheel {
 	return &TimeWheel{
 		interval:    interval,
-		tolerance:   tolerance,
+		divisor:     divisor,
 		scale:       scale,
+		slots:       make([]map[uint64]*Task, scale),
 		addTaskC:    make(chan *Task),
 		removeTaskC: make(chan uint64),
 		stopC:       make(chan bool),
+	}
+}
+
+func (tw *TimeWheel) Start() {
+	tw.ticker = time.NewTicker(tw.interval / time.Duration(tw.divisor))
+	go tw.start()
+}
+
+func (tw *TimeWheel) initSlots() {
+	for i, _ := range tw.slots {
+		tw.slots[i] = make(map[uint64]*Task)
+	}
+}
+
+func (tw *TimeWheel) start() {
+	tw.initSlots()
+
+	for {
+		select {
+		case <-tw.ticker.C:
+			tw.tickHandler()
+			// case tw.addTaskC:
+			// case tw.removeTaskC:
+			// case tw.stopC:
+		}
+	}
+}
+
+func (tw *TimeWheel) tickHandler() {
+	tw.counter++
+
+	log.Println(tw.counter, tw.current)
+	if tw.counter == tw.divisor {
+		tw.counter = 0
+		tw.current++
+		tw.current = tw.current % tw.scale
 	}
 }
