f32ed5fcb94bee0fdae6488e577524e1ae987a79
Ice White <yashiro1899@gmail.com>
2019-02-23 20:46:49 +0800

    1550926009

--- timewheel_test.go
+++ timewheel_test.go
@@ -7,18 +7,18 @@ import (
 )
 
 func f(v ...interface{}) {
-	log.Println(v...)
+	log.Println(time.Now(), v)
 }
 
 func TestTimeWheel_Start(t *testing.T) {
-	tw := NewTimeWheel(10*time.Second, 3600, 10)
+	tw := NewTimeWheel(time.Second, 3600, 20)
 	tw.Start()
-	log.Println("start")
+	log.Println(time.Now())
 
-	task1 := NewTaskAfter([]interface{}{"1s"}, f, time.Second)
-	task5 := NewTaskAfter([]interface{}{"5s"}, f, 5*time.Second)
-	task13 := NewTaskAfter([]interface{}{"13s"}, f, 13*time.Second)
-	task25 := NewTaskAfter([]interface{}{"25s"}, f, 25*time.Second)
+	task1 := NewTaskAfter([]interface{}{"1s"}, f, 130*time.Millisecond)
+	task5 := NewTaskAfter([]interface{}{"5s"}, f, 500*time.Millisecond)
+	task13 := NewTaskAfter([]interface{}{"13s"}, f, 1300*time.Millisecond)
+	task25 := NewTaskAfter([]interface{}{"25s"}, f, 2500*time.Millisecond)
 
 	tw.AddTask(task1)
 	tw.AddTask(task5)
@@ -26,7 +26,7 @@ func TestTimeWheel_Start(t *testing.T) {
 	tw.AddTask(task25)
 	tw.RemoveTask(task5.Id)
 
-	time.Sleep(26 * time.Second)
+	time.Sleep(3 * time.Second)
 	tw.Stop()
 	time.Sleep(time.Second)
 }
